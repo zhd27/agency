ead706b495707c8bd9e41a158bbefc884c70aad4
zhd27 <76961704+zhd27@users.noreply.github.com>
2021-03-14 14:29:20 +0600

    js update

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,13 @@
 
 $(document).ready(function(){
 	
-	$(".navbar-toggler").click(function(){
-	$(".banner_text").toggleClass("problem");
-	});
-			
-	});	
+//	$(".navbar-toggler").click(function(){
+//	$(".banner_text").css({"margin-top":'220px'});			
+//});
 	
-	});
+	$(".navbar-toggler").click(function(){
+	$(".banner_text").toggleClass("problem");			
+    });
+
+	
+});
