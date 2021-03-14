47cc68551325d08cd9938242b34b2149ab73d636
zhd27 <76961704+zhd27@users.noreply.github.com>
2021-03-14 13:45:33 +0600

    jquery update

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,9 @@
 $(document).ready(function(){
 	
 	$(".navbar-toggler").click(function(){
-	$(".banner_text").css({"margin-top":'220px'});			
-});
-
+	$(".banner_text").toggleClass("problem");
+	});
+			
+	});	
 	
-});
+	});
